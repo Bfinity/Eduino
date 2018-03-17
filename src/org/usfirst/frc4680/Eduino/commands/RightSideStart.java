da732ca87bb4d141cc3b9c4e819f05ab38bdb5e8
Ted Hansen <ted@detroitlabs.com>
2018-03-17 19:56:47 -0400

    copy left side auton mode to corresponding right side command

--- src/org/usfirst/frc4680/Eduino/commands/RightSideStart.java
+++ src/org/usfirst/frc4680/Eduino/commands/RightSideStart.java
@@ -2,21 +2,17 @@ package org.usfirst.frc4680.Eduino.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-/**
- *
- */
 public class RightSideStart extends CommandGroup {
 
     public RightSideStart() {
 		addSequential(new FlipWrist());
 		//causes the wrist to flop down after moving forwards then backwards
 		addSequential(new LongArmPosition(LongArmPosition.SWITCH_HEIGHT), 3);
-		
 		addSequential(new DriveABC('R'));
-		addSequential(new TurnTo(-90));
 		addSequential(new SetHeight('R'));
-		addSequential(new DriveDeltaXY(8, 0));
+		addSequential(new TurnTo(-90));
+		addSequential(new DriveDeltaXY(4, 0));
 		addSequential(new Score('R'));
-		
-		}
+		addSequential(new DriveDeltaXY(-10,0));
+    }
 }
